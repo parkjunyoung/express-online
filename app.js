114a6eed25932caedfff29f0d09816bb792ebea9
pjoon86 <pjoon86@naver.com>
2020-03-07 23:48:55 +0900

    View Engine - Nunjucks

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const admin = require('./routes/admin');
 
 const app = express();
 const port = 3000;
+
+nunjucks.configure('template', {
+    autoescape: true,
+    express: app
+});
 
 app.get('/', (req,res) => {
     res.send('express start');
--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,10 @@ router.get('/', (req,res) => {
     res.send('admin app');
 });
 
+router.get('/products', function( _ , res){
+    res.render( 'admin/products.html' , 
+        { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
+    );
+});
+
 module.exports = router;
