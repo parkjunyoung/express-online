91fa3818a22f5885efa7148c5689bc26232b5624
pjoon86 <pjoon86@naver.com>
2020-03-07 23:39:03 +0900

    Routing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const admin = require('./routes/admin');
+
 const app = express();
 const port = 3000;
 
@@ -7,6 +9,9 @@ app.get('/', (req,res) => {
     res.send('express start');
 });
 
+// Routing
+app.use('/admin', admin);
+ 
 app.listen( port, () => {
     console.log('Express listening on port', port);
 });
